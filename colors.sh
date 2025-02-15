ad08647aaae1474b64e606f2ff59da8fe404d03b
Bonface-nayboy <bonnieway615@gmail.com>
2025-02-15 22:26:53 +0300

    Removed tput commands for Render compatibility

--- colors.sh
+++ colors.sh
@@ -5,6 +5,10 @@ tput setaf 2  # Changes text color (remove this)
 tput sgr0     # Resets formatting (remove this)
 
 echo -e "\e[32mThis is green text\e[0m"
+#!/bin/bash
+echo "This is colors.sh"
+echo -e "\e[32mThis is green text\e[0m"  # Green text using ANSI escape codes
+
 
 
 
