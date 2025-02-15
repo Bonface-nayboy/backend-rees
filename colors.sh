e9bb2f024150e9761000b778e1aeca18b0525008
Bonface-nayboy <bonnieway615@gmail.com>
2025-02-15 22:20:32 +0300

    Removed tput commands to fix Render issue

--- colors.sh
+++ colors.sh
@@ -1,2 +1,8 @@
+tput setaf 2   # Change text color (remove this)
+tput sgr0      # Reset text formatting (remove this)
+
 #!/bin/bash
 echo "This is colors.sh"
+echo -e "\e[32mThis is green text\e[0m"
+
+
